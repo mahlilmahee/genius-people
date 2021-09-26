f4ed44cbd64c8e941424fdff056eab80240a543d
acc username <acc email>
2021-09-26 20:47:19 +0600

    all done

--- great-gathering/src/Components/Cart/Cart.js
+++ great-gathering/src/Components/Cart/Cart.js
@@ -13,7 +13,7 @@ const Cart = (props) => {
         <div>
             <h3 className="totaladded"> <FontAwesomeIcon icon={faUser}/> Player added:{cart.length} </h3>
             
-            <h3 className="totaladded"> Total cost:{total}$</h3> 
+            <h3 className="totaladded"> Total cost:{total} $ </h3> 
            {
                cart.map(player=><SelectedPlayer player={player} ></SelectedPlayer>)
            }
